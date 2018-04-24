4c3ee4d7e570c906e54f28e2aa56e07e7062eadc
unknown <carlos.cadilhe@gmail.com>
2018-04-24 00:24:01 -0300

    pasta sql e readme.md

--- sql/contatos_ci2.sql
+++ sql/contatos_ci2.sql
@@ -1,33 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: 24-Abr-2018 às 02:50
--- Versão do servidor: 5.7.19
--- PHP Version: 7.1.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `contatos_ci2`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `contato`
---
-
 DROP TABLE IF EXISTS `contato`;
 CREATE TABLE IF NOT EXISTS `contato` (
   `idContato` int(11) NOT NULL AUTO_INCREMENT,
@@ -48,18 +18,7 @@ CREATE TABLE IF NOT EXISTS `contato` (
   PRIMARY KEY (`idContato`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `contato`
---
 
-INSERT INTO `contato` (`idContato`, `nome`, `sobrenome`, `email`, `rua`, `numero`, `cep`, `bairro`, `cidade`, `estado`, `pais`, `telefone1`, `telefone2`, `telefone3`, `telefone4`) VALUES
-(10, 'Priscilla', 'Santos', 'priscillasantos@gmail.com', 'Rua Estrela da Manhã', '31', '12345-098', 'Mirassol', 'Venhedo', 'SP', 'Brasil', '12992161516', '12997399280', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -70,17 +29,3 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `senha` varchar(255) NOT NULL,
   PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`idUsuario`, `nome`, `sobrenome`, `email`, `senha`) VALUES
-(7, 'Beto', 'Kambene', 'kambene@gmail.com', '2b1faf3b70f4b5fb1d9ad25fe03b8af2'),
-(8, 'Carlos', 'Cadilhe', 'carlos.cadilhe@gmail.com', '260585f280830b62dca769282334cb5f'),
-(9, 'Patricia', 'Lacerda', 'patcacal1212@gmail.com', '2b1faf3b70f4b5fb1d9ad25fe03b8af2');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
